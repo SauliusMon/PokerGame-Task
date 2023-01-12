fae6ce61bd2a4f1096a16ace3bb140316c479fff
SauliusMon <88854073+SauliusMon@users.noreply.github.com>
2023-01-12 13:20:56 +0200

    Init commit

---
+++ src/main/java/com/saulius/poker_game/Main.java
@@ -0,0 +1,8 @@
+package com.saulius.poker_game;
+
+public class Main {
+
+    public static void main (String[] args) {
+
+    }
+}
---
+++ src/test/java/com/saulius/poker_game/PokerGameTest.java
@@ -0,0 +1,19 @@
+package com.saulius.poker_game;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PokerGameTest {
+
+
+    @Test
+    public void testingMain(){
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void testingSomethingElse() {
+        Assertions.fail();
+    }
+
+}
