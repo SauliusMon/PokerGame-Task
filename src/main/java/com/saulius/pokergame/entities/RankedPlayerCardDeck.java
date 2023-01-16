c5c6f374a870acba194dc5df6be22ec340209205
SauliusMon <88854073+SauliusMon@users.noreply.github.com>
2023-01-16 04:24:12 +0200

    Adding ranked / unranked cards when checking hands, comparing fully functional
    TODO - tests, player points

--- src/main/java/com/saulius/pokergame/entities/RankedPlayerCardDeck.java
+++ src/main/java/com/saulius/pokergame/entities/RankedPlayerCardDeck.java
@@ -1,5 +1,7 @@
 package com.saulius.pokergame.entities;
 
+import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
 import java.util.TreeSet;
 
@@ -9,13 +11,11 @@ public class RankedPlayerCardDeck <T extends RankedPlayerCardDeck> implements Co
     private final int deckRanking;
     private final TreeSet<Card> rankedCardsList;
     private final TreeSet<Card> unrankedCardsList;
-    private final PlayerCardDeck playerCardDeck;
 
-    public RankedPlayerCardDeck(int deckRanking, TreeSet<Card> rankedCardsList, TreeSet<Card> unrankedCardsList, PlayerCardDeck playerCardDeck) {
+    public RankedPlayerCardDeck(int deckRanking, TreeSet<Card> rankedCardsList, TreeSet<Card> unrankedCardsList) {
         this.deckRanking = deckRanking;
         this.rankedCardsList = rankedCardsList;
         this.unrankedCardsList = unrankedCardsList;
-        this.playerCardDeck = playerCardDeck;
     }
 
     public int getDeckRanking() {
@@ -30,13 +30,56 @@ public class RankedPlayerCardDeck <T extends RankedPlayerCardDeck> implements Co
         return unrankedCardsList;
     }
 
-    public PlayerCardDeck getPlayerCardDeck() {
-        return playerCardDeck;
-    }
-
-
     @Override
     public int compareTo(T cardDeckObject) {
-        return this.getDeckRanking() - cardDeckObject.getDeckRanking();
+        int handRanking = this.getDeckRanking() - cardDeckObject.getDeckRanking();
+
+        //If the hands aren't of the same ranking, it returns integer
+        if (handRanking != 0) {
+            return handRanking > 1 ? 1 : -1;
+        }
+        //This is done because comparing RoyalFlush always results in a tie, so there is no point to compare card values
+        if (this.getDeckRanking() == 10) {
+                return handRanking;
+        }
+        return this.compareCardSets(cardDeckObject);
     }
+
+    /*
+    Using reverse iterator here, because cards are sorted from smallest to highest
+    */
+    private int compareCardSets (T cardDeckObject) {
+        if (this.getRankedCardsList() != null) {
+            Iterator<Card> firstRankedSetIterator = this.getRankedCardsList().descendingIterator();
+            Iterator secondRankedSetIterator = cardDeckObject.getRankedCardsList().descendingIterator();
+            while (firstRankedSetIterator.hasNext()) {
+                int comparedCards = comparable.compare(firstRankedSetIterator.next(), (Card) secondRankedSetIterator.next());
+                if (comparedCards != 0) {
+                    return comparedCards > 0 ? 1 : -1;
+                }
+            }
+        }
+        if (this.getUnrankedCardsList()!= null) {
+            Iterator<Card> firstUnrankedSetIterator = this.getUnrankedCardsList().descendingIterator();
+            Iterator secondUnrankedSetIterator = cardDeckObject.getUnrankedCardsList().descendingIterator();
+            while (firstUnrankedSetIterator.hasNext()) {
+                int comparedCards = comparable.compare(firstUnrankedSetIterator.next(), (Card) secondUnrankedSetIterator.next());
+                if (comparedCards != 0) {
+                    return comparedCards > 0 ? 1 : -1;
+                }
+            }
+        }
+        return 0;
+    }
+
+    /*
+    This is needed because Cards are being compared by both value and suit.
+    In a case suit matters in CardDeck ranking, it could be deleted and card compare method could be used.
+    */
+    private final Comparator<Card> comparable = new Comparator<Card>() {
+        @Override
+        public int compare(Card card1, Card card2) {
+            return card1.getCardValue() - card2.getCardValue();
+        }
+    };
 }
--- src/main/java/com/saulius/pokergame/hands/PokerHands.java
+++ src/main/java/com/saulius/pokergame/hands/PokerHands.java
@@ -15,6 +15,8 @@ public class PokerHands {
         boolean areCardsOfConsecutiveValues;
 
         TreeSet<Card> cardsTreeSet = playerCardDeck.getDeckOfCards();
+        TreeSet<Card> rankedCards = new TreeSet<>();
+        TreeSet<Card> unrankedCards = new TreeSet<>();
 
         /*
         Finds every single pair, three of a kind, four of a kind and whether cards are of the same suit.
@@ -41,6 +43,12 @@ public class PokerHands {
                     } else {
                         cardValueWithAmountOfAppearances.put(treeSetCard.getCardValue(), 2);
                     }
+                    //Because it appears more than once, adding it to ranked cards set
+                    rankedCards.add(treeSetCard);
+                }
+                else {
+                    //If it doesn't reappear, it means it's unranked or all cards are ranked (Flush, Straight)
+                    unrankedCards.add(treeSetCard);
                 }
                 if (cardSuit != treeSetCard.getCardSuit()) {
                     areCardsOfTheSameSuit = false;
@@ -48,18 +56,6 @@ public class PokerHands {
                 previousCard = treeSetCard;
             }
         }
-//
-//        for (int i = 0; i < cardsTreeSet.size() - 1; i++) {
-//            Card secondCard = cardsTreeSet.get(i + 1);
-//            if (firstCard.getCardValue() == secondCard.getCardValue()) {
-//                int valueOfPair = firstCard.getCardValue();
-//                if (cardValueWithAmountOfAppearances.containsKey(valueOfPair)) {
-//                    cardValueWithAmountOfAppearances.replace(valueOfPair, cardValueWithAmountOfAppearances.get(valueOfPair) + 1);
-//                } else {
-//                    cardValueWithAmountOfAppearances.put(firstCard.getCardValue(), 2);
-//                }
-//            }
-//        }
         
         /*
         Checks if it's a Royal Flush
@@ -67,7 +63,7 @@ public class PokerHands {
         int smallestCardValue = cardsTreeSet.first().getCardValue();
         int highestCardValue = cardsTreeSet.last().getCardValue();
         if (smallestCardValue == 10 && highestCardValue == 14 && areCardsOfTheSameSuit) {
-            return new RankedPlayerCardDeck(10, new TreeSet<>(), new TreeSet<>(), playerCardDeck);
+            return new RankedPlayerCardDeck(10, playerCardDeck.getDeckOfCards(), new TreeSet<>());
         }
 
         /*
@@ -79,15 +75,17 @@ public class PokerHands {
         Checks if it's a Straight Flush
         */
         if (areCardsOfTheSameSuit && areCardsOfConsecutiveValues) {
-            return new RankedPlayerCardDeck(9, new TreeSet<>(), new TreeSet<>(), playerCardDeck);
+            return new RankedPlayerCardDeck(9, playerCardDeck.getDeckOfCards(), new TreeSet<>());
         }
+
         /*
         Checks if it's a Four of a Kind
         Same value reappears 4 times in a CardDeck
+        In this case there is no point to know the value of last card.
+        Reason being, four of a kind can't be of the same value for both players
          */
-
         if (cardValueWithAmountOfAppearances.containsValue(4)) {
-            return new RankedPlayerCardDeck(8, new TreeSet<>(), new TreeSet<>(), playerCardDeck);
+            return new RankedPlayerCardDeck(8, rankedCards, new TreeSet<>());
         }
 
         /*
@@ -96,46 +94,48 @@ public class PokerHands {
         In a case its only 2 reappearing values, it's Two pairs.
          */
         if (cardValueWithAmountOfAppearances.size() == 2 && cardValueWithAmountOfAppearances.containsValue(3)) {
-            return new RankedPlayerCardDeck(7, new TreeSet<>(), new TreeSet<>(), playerCardDeck);
+            return new RankedPlayerCardDeck(7, playerCardDeck.getDeckOfCards(), new TreeSet<>());
 
         }
         /*
         Checks if it's a Flush
         */
         if (areCardsOfTheSameSuit) {
-            return new RankedPlayerCardDeck(6, new TreeSet<>(), new TreeSet<>(), playerCardDeck);
+            return new RankedPlayerCardDeck(6, playerCardDeck.getDeckOfCards(), new TreeSet<>());
 
         }
         /*
         Checks if it's a Straight
         */
         if (areCardsOfConsecutiveValues) {
-            return new RankedPlayerCardDeck(5, new TreeSet<>(), new TreeSet<>(), playerCardDeck);
+            return new RankedPlayerCardDeck(5, playerCardDeck.getDeckOfCards(), new TreeSet<>());
 
         }
         /*
         Checks if it's a Three of a Kind
+        For Three of a kind to have same value in both player hands, there should be 6 same value unique cards - which is impossible
         */
         if (cardValueWithAmountOfAppearances.containsValue(3)) {
-            return new RankedPlayerCardDeck(4, new TreeSet<>(), new TreeSet<>(), playerCardDeck);
+            return new RankedPlayerCardDeck(4, rankedCards, new TreeSet<>());
 
         }
         /*
         Checks if it's Two Pairs
         */
         if (cardValueWithAmountOfAppearances.size() == 2) {
-            return new RankedPlayerCardDeck(3, new TreeSet<>(), new TreeSet<>(), playerCardDeck);
+            return new RankedPlayerCardDeck(3, rankedCards, unrankedCards);
         }
         /*
         Checks if it's One Pair
         */
         if (cardValueWithAmountOfAppearances.size() != 0) {
-            return new RankedPlayerCardDeck(2, new TreeSet<>(), new TreeSet<>(), playerCardDeck);
+            return new RankedPlayerCardDeck(2, rankedCards, unrankedCards);
 
         }
         /*
         Returns 1 if it's a High Card
+        If it's a High Card, it means all Cards are unranked.
         */
-        return new RankedPlayerCardDeck(1, new TreeSet<>(), new TreeSet<>(), playerCardDeck);
+        return new RankedPlayerCardDeck(1, new TreeSet<>(), playerCardDeck.getDeckOfCards());
     }
 }
